e3618756a7c03dd9aa30819c7c8698d548211d9a
KUSHALDSMaster20 <kushal998081@gmail.com>
2026-09-25 09:15:59 +0530

    cp:8

--- Day/Day3/CaptoneProject/app/main.py
+++ Day/Day3/CaptoneProject/app/main.py
@@ -1,26 +1,33 @@
-# this file is the "entry point of the application"
+# This file is the "entry point" of the application
 from fastapi import FastAPI
 
 from app.config import settings
 from app.database import ping_database
 from app.routers import users
+from app.routers import categories
+from app.routers import tickets
+from app.routers import comments
+from app.routers import audit_logs
 
 
-# creating fastdAPI instance
-app= FastAPI(title=settings.APP_NAME)
+# Creating FastAPI app instance
+app = FastAPI(title=settings.APP_NAME)
 
 app.include_router(users.router)
+app.include_router(categories.router)
+app.include_router(tickets.router)
+app.include_router(comments.router)
+app.include_router(audit_logs.router)
 
-# this  function runs once when the server starts. It checks the DB connections
-
+# This function runs once when the server starts. It checks the DB connection.
 @app.on_event("startup")
-def on_startup()-> None:
+def on_startup() -> None:
     if not ping_database():
         raise RuntimeError("Could not connect to MongoDB")
     print(f"[startup]Connected to MongoDB. App:{settings.APP_NAME}")
 
-# checks basic health-check API endpoint & confirms 
-# GET / is running & reachable. (/ is considerd as 'root')
-@app.get("/",tags=["health"])
+# Checks basic health-check API endpoint & confirms 
+# GET / is running & reachable. (/ is considered as 'root')
+@app.get("/",tags=["Health"])
 def health_check():
     return {"status":"ok","app":settings.APP_NAME}
